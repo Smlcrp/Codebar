0a3136060cdab133341dbce6283c65ef64d7800b
Smlcrp <smlcrp@gmail.com>
2022-02-02 16:46:21 -0500

    created workshop class, still needs more work

--- codebar.py
+++ codebar.py
@@ -6,11 +6,10 @@ class Member:
         print(f"Hi! I am {self.name} and I am a {self.job} at Codebar!")
 
 
+
 class Student(Member):
     def __init__(self, name, reason, job = 'Student'):
         super().__init__(name, job)
-    def stu_records(self):
-        print(f"My name is {self.name}, I am a {self.job} at Codebar and I came here because {self.reason}")
 
 
 
@@ -22,7 +21,20 @@ class Teacher(Member):
         self.skills += skill
 
 
+
+class Workshop:
+    def __init__(self, date, subject):
+        self.date = date
+        self.subject = subject
+        self.instructors = instructors
+        self.students = students
+
+    def add_participant(self):
+        
+
+
 sam = Student('Sam', 'For Fun')
 eric = Teacher('Eric', 'I Like to code a lot')
+
 sam.greet()
 eric.greet()
