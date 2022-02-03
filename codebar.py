cc93cbe6a7e18377b72507a0876dfa89e328b1b9
Smlcrp <smlcrp@gmail.com>
2022-02-03 11:29:16 -0500

    Finished codebar lab

--- codebar.py
+++ codebar.py
@@ -10,15 +10,18 @@ class Member:
 class Student(Member):
     def __init__(self, name, reason, job = 'Student'):
         super().__init__(name, job)
+        self.reason = reason
 
 
 
 class Teacher(Member):
-    def __init__(self, name, bio, skills = 'Python, Javascript', job = 'Teacher'):
+    def __init__(self, name, bio, skills = [], job = 'Teacher'):
         super().__init__(name, job)
+        self.skills = skills
+        self.bio = bio
     
     def add_skill(self, skill):
-        self.skills += skill
+        self.skills.append(skill)
 
 
 
@@ -26,15 +29,46 @@ class Workshop:
     def __init__(self, date, subject):
         self.date = date
         self.subject = subject
-        self.instructors = instructors
-        self.students = students
+        self.teachers = []
+        self.students = []
 
-    def add_participant(self):
+    def add_participant(self, member):
+        self.member = member
+        if (type(member).__name__) == 'Student':
+            self.students.append(member)
+        elif (type(member).__name__) == 'Teacher':
+            self.teachers.append(member)
+        else:
+            print("Not Valid Member")
+
+    def print_details(self):
+        print(f'Workshop: {self.subject}  Startdate: {self.date}')
+        print("Students")
+        for index, Student in enumerate(self.students):
+            print(f"{index + 1}: {Student.name} \n {Student.reason}")
+        print("Teachers")
+        for index, Teacher in enumerate(self.teachers):
+            print(f"{index + 1}: {Teacher.name} \n {Teacher.skills} \n {Teacher.bio}")
+
+
+
         
 
 
-sam = Student('Sam', 'For Fun')
-eric = Teacher('Eric', 'I Like to code a lot')
+sam = Student('Sam', 'For Fun and to be smart')
+eric = Teacher('Eric', 'I Like to code a lot, and want to shape the minds of students')
 
-sam.greet()
-eric.greet()
+workshop = Workshop('12/03/2014', 'Shutl')
+jane = Student('Jane Doe', 'I am trying to learn programming and need some help')
+lena = Student('Lena Smith', 'I am really excited about learning to program!')
+vicky = Teacher('Vicky Python', 'I want to help people learn coding.')
+vicky.add_skill('HTML')
+vicky.add_skill('JavaScript')
+nicole = Teacher('Nicole McMillan', 'I have been programming for 5 years in Python and want to spread the love')
+nicole.add_skill('Python')
+
+workshop.add_participant(jane)
+workshop.add_participant(lena)
+workshop.add_participant(vicky)
+workshop.add_participant(nicole)
+workshop.print_details()
